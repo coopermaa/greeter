e138e877c7663a425307d04dbf3de248f5ac4cf7
Cooper Maa <coopermaa77@gmail.com>
2016-08-31 20:37:21 +0800

    Rendering HTML using ERB

--- greeter.rb
+++ greeter.rb
@@ -1,6 +1,13 @@
 # greeter.rb
+require 'erb'
+
 class Greeter
   def call(env)
-    Rack::Response.new "Hello World"
+    Rack::Response.new render("index.erb")
   end
+  
+  def render(template)
+    path = File.expand_path("../views/#{template}", __FILE__)
+    ERB.new(File.read(path)).result
+  end  
 end
